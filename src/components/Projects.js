d8bacbbc3d84b435b764179ea1886f7afe8d8f7f
Yash Vasantbhai Prajapati <1216503@TCSAVIS.COM>
2018-09-16 15:01:55 +0530

    modified in project.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Grid, Cell } from 'react-mdl';
 
 class Projects extends React.Component {
   render() {
     return (
-      <div><h1>Projects</h1></div>
+      <div>
+        <Grid className="project-grid">
+          <Cell col={12} className="project-cell">
+            <h1><b>My Projects</b></h1>
+            <p>The Website you are looking at is the result of the same. The website highlights the details about my career.</p>
+          </Cell>
+          <Cell col={12} className="project-cell">
+            <h1><b>Facebook Movie bot</b></h1>
+            <p>A bot which when provided with a word/movie name brings you the list of movies relating to that keyword.</p>
+            <p>When user selects the particular option from the list, the bot reverts back with the details of that movie.</p>
+          </Cell>
+        </Grid>
+      </div>
     );
   }
 }
