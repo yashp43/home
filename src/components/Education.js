77d6ecc3070b4e2ed322ff368184f25162176fc3
Yash Prajapati <yashp43@gmail.com>
2018-09-28 02:02:53 +0530

    modified education indentation

--- src/components/Education.js
+++ src/components/Education.js
@@ -5,10 +5,10 @@ class Education extends Component {
   render() {
     return (
       <Grid>
-        <Cell col={4}>
+        <Cell col={3}>
           <p>{this.props.startYear} - {this.props.endYear}</p>
         </Cell>
-        <Cell col={8}>
+        <Cell col={9}>
           <h4 style={{marginTop:'0px', marginBottom:'0px'}}>{this.props.schoolName}</h4>
           <p><i>{this.props.degree}</i></p>
         </Cell>
